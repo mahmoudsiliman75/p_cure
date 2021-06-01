77dc974eaad000ee75ca6c39f77d7b89609a5722
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-06-01 16:36:16 +0200

    add home page faqs section

--- index.php
+++ index.php
@@ -511,6 +511,107 @@
 </div>
 <!-- END:: VIDEOS SECTION -->
 
+<!-- START:: FAQS SECTION -->
+<div class="faq_sec">
+  <div class="container">
+    <div class="header_wraper">
+      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+        FAQs 
+      </h2>
+    </div>
+
+    <div class="row justify-content-center overflow-hidden">
+      <div class="col-12 col-md-6 my-3 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1s">
+        <div class="heighlight_faqs_wraper">
+
+          <div class="heighlight_faq_card my-3">
+            <h3> Important Question 1 ? </h3>
+            <div class="answer">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Consequatur facilis ex, animi, vero laborum corporis assumenda 
+              totam, minima impedit doloribus fugiat eaque optio voluptates.
+            </div>
+          </div>
+
+          <div class="heighlight_faq_card my-3">
+            <h3> Important Question 2 ? </h3>
+            <div class="answer">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Consequatur facilis ex, animi, vero laborum corporis assumenda 
+              totam, minima impedit doloribus fugiat eaque optio voluptates.
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 my-3 wow fadeInDown" data-wow-duration="1.5s" data-wow-delay="1.5s">
+        <div class="faqs_wraper">
+          <div class="accordion" id="faqs_accordion">
+
+            <div class="card border my-3">
+              <div class="card-header" id="headingOne">
+                <h4 class="mb-0">
+                  <button class="btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <i class="fas fa-angle-double-right"></i>  
+                    Question #1
+                  </button>
+                </h4>
+              </div>
+
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#faqs_accordion">
+                <div class="card-body">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Reprehenderit minima mollitia explicabo unde et illo illum. 
+                  Quia vel, culpa sequi consequuntur neque illo porro facere 
+                </div>
+              </div>
+            </div>
+
+            <div class="card border my-3">
+              <div class="card-header" id="headingTwo">
+                <h4 class="mb-0">
+                  <button class="btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <i class="fas fa-angle-double-right"></i>
+                    Question #2
+                  </button>
+                </h4>
+              </div>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#faqs_accordion">
+                <div class="card-body">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Reprehenderit minima mollitia explicabo unde et illo illum. 
+                  Quia vel, culpa sequi consequuntur neque illo porro facere 
+                </div>
+              </div>
+            </div>
+
+            <div class="card border my-3">
+              <div class="card-header" id="headingThree">
+                <h4 class="mb-0">
+                  <button class="btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fas fa-angle-double-right"></i>
+                    Quesion #3
+                  </button>
+                </h4>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#faqs_accordion">
+                <div class="card-body">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Reprehenderit minima mollitia explicabo unde et illo illum. 
+                  Quia vel, culpa sequi consequuntur neque illo porro facere 
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: FAQS SECTION -->
+
 <div class="bg_wraper_2">
   <!-- START:: ARTICLES SECTION -->
   <div class="articles_sec">
